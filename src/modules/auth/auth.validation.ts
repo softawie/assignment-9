75bb5082737dcb809451a6de6d3e934bf67498ca
emkc0078 <ytaha@emkanfinco.com.sa>
2025-09-09 21:05:39 +0300

    enhanc: general validations

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -1,60 +1,22 @@
-import { UserRoles } from "@utils/enums";
+import { generalValidations } from "@utils/general.valiations";
 import joi from "joi";
 
 const signUpValidation = joi
   .object({
-    firstName: joi.string().min(3).max(50).required().messages({
-      "string.min": "Name must be at least 3 characters long",
-      "string.max": "the {VALUE} must be at most 50 characters long",
-      "any.required": "firstName is required",
-    }),
-    lastName: joi.string().required().messages({
-      "string.min": "Name must be at least 3 characters long",
-      "string.max": "the {VALUE} must be at most 50 characters long",
-      "any.required": "lastName is required",
-    }),
-    email: joi.string().email().required().messages({
-      "string.email": "email is required",
-      "any.required": "email is required",
-    }),
-    password: joi.string().required().messages({
-      "any.required": "password is required",
-    }),
-    age: joi.number().required().messages({
-      "any.required": "age is required",
-    }),
-    phone: joi.string().messages({
-      "any.required": "phone must be a string",
-    }),
-    role: joi
-      .string()
-      .valid(...Object.values(UserRoles).filter((v) => typeof v === "string"))
-      .required()
-      .messages({
-        "any.required": "role is required",
-        "any.allowOnly": "role is not supported",
-      })
-      .default(UserRoles.USER),
+    firstName: generalValidations.firstName.required(),
+    lastName: generalValidations.lastName.required(),
+    email: generalValidations.email.required(),
+    password: generalValidations.password.required(),
+    age: generalValidations.age,
+    phone: generalValidations.phone,
+    role: generalValidations.role,
   })
 
 
 const loginValidation = joi
   .object({
-    email: joi
-      .string()
-      .email({
-        minDomainSegments: 2,
-        maxDomainSegments: 3,
-        tlds: { allow: ["com", "net", "org", "gmail"] },
-      })
-      .required()
-      .messages({
-        "string.email": "email is required",
-        "any.required": "email is required",
-      }),
-    password: joi.string().required().messages({
-      "any.required": "password is required",
-    }),
+    email: generalValidations.email.required(),
+    password: generalValidations.password.required(),
   })
   .required();
 
---
+++ src/utils/general.valiations.ts
@@ -0,0 +1,33 @@
+import { UserRoles } from "@utils/enums";
+import joi from "joi";
+
+export const generalValidations = {
+        firstName: joi.string().min(3).max(50).messages({
+          "string.min": "Name must be at least 3 characters long",
+          "string.max": "the {VALUE} must be at most 50 characters long",
+        }),
+        lastName: joi.string().messages({
+          "string.min": "Name must be at least 3 characters long",
+          "string.max": "the {VALUE} must be at most 50 characters long",
+        }),
+        email: joi.string().email().messages({
+          "string.email": "email is required",
+        }),
+        password: joi.string().messages({
+          "any.required": "password is required",
+        }),
+        age: joi.number().messages({
+          "any.required": "age is required",
+        }),
+        phone: joi.string().messages({
+          "any.required": "phone must be a string",
+        }),
+        role: joi
+          .string()
+          .valid(...Object.values(UserRoles).filter((v) => typeof v === "string"))
+          .messages({
+            "any.required": "role is required",
+            "any.allowOnly": "role is not supported",
+          })
+          .default(UserRoles.USER),
+}
